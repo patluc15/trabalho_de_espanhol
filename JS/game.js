53cfe7da965dd6214be63e5487cb50e5ea6a92cc
patluc15 <belinhalg15@gmail.com>
2025-02-02 18:32:48 -0300

    Back-end do jogo completo

--- JS/game.js
+++ JS/game.js
@@ -13,9 +13,21 @@ const verbos = [
     'conduzca',
     'elija',
     'destruye',
+    'oid2',
+    'traigan2',
+    've2',
+    'construyan2',
+    'conoced2',
+    'cae2',
+    'seguid2',
+    'siente2',
+    'conduzca2',
+    'elija2',
+    'destruye2',
 ];
 
 const create_element = (tag, className) => {
+
 
     const element = document.createElement(tag);
     element.className = className;
@@ -23,6 +35,74 @@ const create_element = (tag, className) => {
 
 }
 
+let first_card = '';
+let second_card = '';
+
+const check_endgame = () => {
+    const disabled_cards = document.querySelectorAll('.disabled-card');
+
+    if (disabled_cards.length == 22){
+        alert('Parabéns, voccê completou o jogo');
+    }
+
+}
+
+const check_cards = () => {
+
+    let first_verb = first_card.getAttribute('data-verb');
+    let second_verb = second_card.getAttribute('data-verb');
+
+    if (first_verb == second_verb + '2' || second_verb == first_verb + '2'){
+        
+        first_card.firstChild.classList.add('disabled-card');
+        second_card.firstChild.classList.add('disabled-card');
+
+        first_card = '';
+        second_card = '';
+
+        check_endgame();
+
+    }
+    else{
+
+        setTimeout(() => {
+            
+            first_card.classList.remove('reveal-card');
+            second_card.classList.remove('reveal-card');
+        
+            first_card = '';
+            second_card = '';
+
+        }, 1000);
+
+    }
+
+}
+
+const reveal_card = ( { target } ) => {
+
+    if (target.parentNode.className.includes('reveal-card')){
+
+        return;
+
+    }
+
+    if (first_card == ''){
+
+        target.parentNode.classList.add('reveal-card');
+        first_card = target.parentNode;
+    }
+    else if (second_card == ''){
+
+        target.parentNode.classList.add('reveal-card');
+        second_card = target.parentNode;
+
+        check_cards();
+
+    }
+    
+
+}
 
 const create_card = (verbo) => {
 
@@ -35,6 +115,9 @@ const create_card = (verbo) => {
     card.appendChild(front);
     card.appendChild(back);
 
+    card.addEventListener('click', reveal_card);
+    card.setAttribute('data-verb', verbo);
+
     grid.appendChild(card);
 
     return card;
@@ -42,14 +125,19 @@ const create_card = (verbo) => {
 
 const load_game = () => {
 
-    verbos.forEach((verbo) => {
+    const shuffled_array = verbos.sort(() => Math.random() - 0.5);
+
+    shuffled_array.forEach((verbo) => {
 
         const card = create_card(verbo);
         grid.appendChild(card);
+
+        
 
     });
 
 }
 
 load_game();
+
 
